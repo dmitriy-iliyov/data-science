23f997778624422d24511f9a99718dd83b087f27
sayner <sayner@sayners-mbp.speedport.ip>
2024-12-07 17:08:07 +0100

    fix(change parser and add write_to_csv() method from lab_6/tools)

--- general/lab_1/parser.py
+++ general/lab_1/parser.py
@@ -2,30 +2,48 @@ import datetime
 import requests
 import pandas as pd
 
+from lab_6.tools import filer
 
-def coin_parsing(currency, days=365):
+
+def coin_parsing(currency, days=90, interval='hourly'):
+
+    filepath = ("/Users/sayner/github_repos/data-science/general/lab_1/files/" + str(currency).lower() + "_"
+                + interval + ".csv")
+
     test_url = 'https://api.coingecko.com/api/v3/coins/' + currency
     test_response = requests.get(test_url)
+
     if test_response.status_code == 200:
+
         print('Test request successful')
         url = test_url + '/market_chart'
+
         params = {
             'vs_currency': 'usd',
-            'days': days,
-            'interval': 'daily'
+            'days': days
         }
 
+        if interval != 'hourly':
+            params['interval'] = interval
+
         response = requests.get(url, params=params)
+
+        if response.status_code == 429:
+            return pd.read_csv(filepath)
+
         coin_year_prices = response.json()
         dates = []
         prices = []
+
         for daily_price in coin_year_prices['prices']:
             dates.append(datetime.datetime.fromtimestamp(daily_price[0] / 1000))
             prices.append(daily_price[1])
         coin_year_prices_df = pd.DataFrame({'date': dates, 'price': prices})
-        coin_year_prices_df.to_csv('files/' + currency + '.csv', index=True)
+
+        filer.write_to_csv(filepath, coin_year_prices_df)
         print('Coin parsing successful')
-        return coin_year_prices_df
+
+        return pd.read_csv(filepath)
     else:
         print('Test request failed, status code ' + str(test_response.status_code))
         print(test_response.text)
