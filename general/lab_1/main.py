e2fea7039eccf6961765e86685c959a00c995f5d
sayner <sayner@sayners-MacBook-Pro.local>
2024-12-09 22:35:51 +0100

    fix(# coin_parsing)

--- general/lab_1/main.py
+++ general/lab_1/main.py
@@ -5,11 +5,10 @@ import statistic_learning as sl
 
 
 currency = 'bitcoin'
-# df = parser.coin_parsing(currency, 365)
+# df = parser.coin_parsing(currency, 365, 'daily')
 df = pd.read_csv('files/' + currency + '.csv')
 sample = df['price']
 analyzer = sl.CryptoAnalyzer(sample, currency)
 # analyzer.lsm_approximation()
 # analyzer.lsm_extrapolation(365)
 # analyzer.model(365, True, True)
-analyzer.detected_anomalies_LSM()
