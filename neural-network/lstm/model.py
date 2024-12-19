eea0a084e387e531f37768513259d5a62dc7977f
sayner <sayner@sayners-mbp.speedport.ip>
2024-12-19 04:05:38 +0100

    fix(changes in model arhitecture, training and plotting)

--- neural-network/lstm/model.py
+++ neural-network/lstm/model.py
@@ -1,64 +1,74 @@
-
 import time
 
 from keras.src.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Input, LSTM
+from tensorflow.keras.layers import Dense, LSTM, Embedding, Input, Dropout
 from matplotlib import pyplot as plt
 
 
 class KerasLSTM:
 
-    def __init__(self):
+    def __init__(self, word_count, max_length):
         self._model = Sequential([
-            Input(shape=(24, 1)),
+            Input(shape=(max_length,)),
+            Embedding(input_dim=word_count + 1, output_dim=128, input_length=max_length),
             LSTM(128, activation='tanh', return_sequences=True),
-            LSTM(128, activation='tanh', return_sequences=False),
-            Dense(1, activation='linear')
+            #Dropout(0.2),
+            LSTM(64, activation='tanh', return_sequences=False),
+            # Dropout(0.2),
+            Dense(5, activation='softmax')
         ])
-        self._model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+        self._model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
     def summary(self):
         self._model.summary()
 
-    def fit(self, train_data, train_answers, val_data=None, val_answers=None, epochs=100, batch_size=32):
+    def fit(self, train_data, train_answers, validation_split=0.1, epochs=100, batch_size=256):
         start_time = time.time()
         early_stopping = EarlyStopping(
-            monitor='val_loss',
-            patience=5,
+            monitor='val_accuracy',
+            patience=1,
             restore_best_weights=True
         )
         history = self._model.fit(
             train_data, train_answers,
-            validation_data=(val_data, val_answers) if val_data is not None else None,
             epochs=epochs,
             batch_size=batch_size,
-            verbose=1,
+            validation_split=validation_split,
             callbacks=[early_stopping]
         )
         execution_time = time.time() - start_time
         print(f"Training completed in {execution_time:.2f} seconds.")
-        self.plot_history(history, epochs, execution_time)
+        self.plot_history(history, execution_time)
+        self.save()
         return history, execution_time
 
     def evaluate(self, test_data, test_answers):
-        test_loss, test_mae = self._model.evaluate(test_data, test_answers, verbose=1)
-        return test_loss, test_mae
+        test_loss, test_accuracy = self._model.evaluate(test_data, test_answers, verbose=1)
+        print(f"Test Loss: {test_loss}, Test Accuracy: {test_accuracy}")
+        return test_loss, test_accuracy
 
     def predict(self, sequence):
         return self._model.predict(sequence)
 
+    def save(self, path='/content/drive/MyDrive/main/languages/Python/neural_network/labs/lab_7/model/lstm_model.keras'):
+        self._model.save(path)
+
     @staticmethod
-    def plot_history(history, epochs, execution_time):
+    def plot_history(history, execution_time):
+        epochs = len(history.history['loss'])
         plt.figure(figsize=(12, 4))
+
         plt.subplot(1, 2, 1)
-        plt.plot(range(1, epochs + 1), history.history['mae'], label='Training MAE')
-        if 'val_mae' in history.history:
-            plt.plot(range(1, epochs + 1), history.history['val_mae'], label='Validation MAE')
+        plt.plot(range(1, epochs + 1), history.history['accuracy'], label='Training Accuracy')
+        if 'val_accuracy' in history.history:
+            plt.plot(range(1, epochs + 1), history.history['val_accuracy'], label='Validation Accuracy')
         plt.xlabel('Epochs')
-        plt.ylabel('Mean Absolute Error')
-        plt.title(f'MAE (Execution Time: {execution_time:.2f} seconds)')
+        plt.ylabel('Accuracy')
+        plt.title(f'Accuracy (Execution Time: {execution_time:.2f} seconds)')
         plt.legend()
+
         plt.subplot(1, 2, 2)
         plt.plot(range(1, epochs + 1), history.history['loss'], label='Training Loss')
         if 'val_loss' in history.history:
@@ -70,3 +80,4 @@ class KerasLSTM:
 
         plt.tight_layout()
         plt.show()
+
