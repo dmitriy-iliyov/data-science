1aefeed353017383084fdea8048b097b8adb7e42
sayner <sayner@sayners-MacBook-Pro.local>
2024-12-22 02:38:15 +0100

    del(del lab_2)

--- lab_2/main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from src.crypto_clusterizator import CryptoClusterizator
-
-n_clusters = 3
-crypto_clusterizer = CryptoClusterizator(n_clusters)
-crypto_clusterizer.clusterize('volatility', 'total_supply')
